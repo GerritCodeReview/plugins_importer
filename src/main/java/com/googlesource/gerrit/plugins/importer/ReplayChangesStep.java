be3f1c66f385903210b135b0e1ddf31f6a6ba70c
Edwin Kempin <edwin.kempin@sap.com>
2015-04-15 14:05:30 +0200

    Do not fail if change has no revisions
    If a change has no revisions this failed on change update with
    
    com.google.gwtorm.server.OrmException: insert failure on changes
      at com.google.gwtorm.schema.sql.SqlDialect.convertError(SqlDialect.java:158)
      at com.google.gwtorm.schema.sql.DialectMySQL.convertError(DialectMySQL.java:232)
      at com.google.gwtorm.jdbc.JdbcAccess.convertError(JdbcAccess.java:459)
      at com.google.gwtorm.jdbc.JdbcAccess.insert(JdbcAccess.java:170)
      at com.google.gwtorm.jdbc.JdbcAccess.upsert(JdbcAccess.java:332)
      at com.googlesource.gerrit.plugins.importer.ReplayChangesStep.upsertChange(ReplayChangesStep.java:214)
      at com.googlesource.gerrit.plugins.importer.ReplayChangesStep.replayChange(ReplayChangesStep.java:163)
      at com.googlesource.gerrit.plugins.importer.ReplayChangesStep.replay(ReplayChangesStep.java:133)
      at com.googlesource.gerrit.plugins.importer.ImportProject.apply(ImportProject.java:239)
      at com.googlesource.gerrit.plugins.importer.ImportProject.apply(ImportProject.java:174)
      ...
    Caused by: java.sql.BatchUpdateException: Column 'subject' cannot be null
      ...
    
    Ignore changes without revisions and log them as a warning.
    
    Change-Id: Ib368931a427a58485e6e68014f9b4d1441949206
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- src/main/java/com/googlesource/gerrit/plugins/importer/ReplayChangesStep.java
+++ src/main/java/com/googlesource/gerrit/plugins/importer/ReplayChangesStep.java
@@ -17,6 +17,7 @@ package com.googlesource.gerrit.plugins.importer;
 import com.google.common.collect.Iterators;
 import com.google.gerrit.common.Nullable;
 import com.google.gerrit.common.errors.NoSuchAccountException;
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.client.ChangeStatus;
 import com.google.gerrit.extensions.common.ChangeInfo;
 import com.google.gerrit.extensions.restapi.BadRequestException;
@@ -64,6 +65,7 @@ class ReplayChangesStep {
 
   private static Logger log = LoggerFactory.getLogger(ReplayChangesStep.class);
 
+  private final String pluginName;
   private final ReplayRevisionsStep.Factory replayRevisionsFactory;
   private final ReplayInlineCommentsStep.Factory replayInlineCommentsFactory;
   private final ReplayMessagesStep.Factory replayMessagesFactory;
@@ -86,6 +88,7 @@ class ReplayChangesStep {
 
   @Inject
   ReplayChangesStep(
+      @PluginName String pluginName,
       ReplayRevisionsStep.Factory replayRevisionsFactory,
       ReplayInlineCommentsStep.Factory replayInlineCommentsFactory,
       ReplayMessagesStep.Factory replayMessagesFactory,
@@ -105,6 +108,7 @@ class ReplayChangesStep {
       @Assisted("resume") boolean resume,
       @Assisted ResumeImportStatistic importStatistic,
       @Assisted ProgressMonitor pm) {
+    this.pluginName = pluginName;
     this.replayRevisionsFactory = replayRevisionsFactory;
     this.replayInlineCommentsFactory = replayInlineCommentsFactory;
     this.replayMessagesFactory = replayMessagesFactory;
@@ -170,6 +174,13 @@ class ReplayChangesStep {
         return;
       }
     }
+
+    if (c.revisions.isEmpty()) {
+      log.warn(String.format("[%s] Change %s has no revisions.",
+          pluginName, c.id));
+      return;
+    }
+
     replayRevisionsFactory.create(repo, rw, change, c).replay(api);
     upsertChange(resumeChange, change, c);
 
